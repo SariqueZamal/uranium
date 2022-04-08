206452ed63ba53ed038ae3b5dba343d61c94b6d4
SariqueZamal <sariquezamal0@gmail.com>
2022-04-08 17:04:04 +0530

    missingNumber

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,30 @@
 const express = require('express');
-const logger = require('./logger')
+
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
+router.get('/question1', function(req, res) {
+    let arr= [1,2,3,5,6,7,8,9]
+ 
+   let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+     sum += arr[i]; 
+  }
+   let missingNumber= (((arr.length+1) * (arr.length+2)) / 2) - sum
+ 
+   res.send(  { "The Number is": missingNumber  }  );
 })
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/question2', function (req, res) {
+    let arr= [13,14,15,17,18,19]
+
+    let sum = 0;
+   for (let i = 0; i < arr.length; i++) {
+      sum += arr[i]; 
+   }
+    let missingNumber= (arr.length+1) * ((arr[0])+(arr[arr.length-1])) / 2 - sum
+  
+    res.send(  { "The Number is": missingNumber  }  );
 });
 
 
