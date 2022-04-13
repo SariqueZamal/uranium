0ff993a6a778fb681aa74e265810d0eba96a12cd
SariqueZamal <sariquezamal0@gmail.com>
2022-04-13 12:42:20 +0530

    assignment/mongoDb-day2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,19 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
+
+    bookName:{type: String, required: true},
     authorName: String, 
     tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
+    totalPages: Number,
+    stockAvailable: Boolean,
+    price: {
         indianPrice: String,
-        europePrice: String,
+        europeanPrice: String,  
     },
-    sales: {type: Number, default: 10}
+    year: {type: Number, default: 2021}
+
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports = mongoose.model('Book', bookSchema) //books
